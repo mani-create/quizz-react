474d2867d97544aea9941d4e3b57a4d75f730834
mani-create <manikandaraj997@gmail.com>
2021-01-01 19:52:01 +0530

    changes done!

--- src/components/QuizMain.js
+++ src/components/QuizMain.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Question from './question/Question';
 import Answer from './answer/Answer';
 import './QuizMain.css';
@@ -8,31 +8,45 @@ export default class Quiz extends Component {
     // initiating the local state
     state = {
         quiestions: {
-            1: 'What US city is known as the "birthplace of jazz"?',
-            2: 'What is the capital of Greece?',
-            3: 'What planet gave birth to Superman?'
+            1: "India is a federal union comprising twenty-nine states and how many union territories?",
+            2: "Which of the following is the capital of Arunachal Pradesh?",
+            3: "Which of the following states is not located in the North?",
+            4: "In which state is the main language Khasi?",
+            5: "In what state is the Elephant Falls located?",
         },
         answers: {
             1: {
-                1: 'Chicago',
-                2: 'New Orleans',
-                3: 'New York'
+                1: "6",
+                2: "7",
+                3: "8",
             },
             2: {
-                1: 'Athens',
-                2: 'Patras',
-                3: 'Kalamata'
+                1: "Itanagar",
+                2: "Dispur",
+                3: "Imphal",
             },
             3: {
-                1: 'Krypton',
-                2: 'Mars',
-                3: 'Saturn'
-            }
+                1: "Jharkhand",
+                2: "Jammu and Kashmir",
+                3: "Himachal Pradesh",
+            },
+            4: {
+                1: "Mizoram",
+                2: "Nagaland",
+                3: "Meghalaya",
+            },
+            5: {
+                1: "Mizoram",
+                2: "Meghalaya",
+                3: "Manipur",
+            },
         },
         correctAnswers: {
-            1: '2',
-            2: '1',
-            3: '1'
+            1: "2",
+            2: "1",
+            3: "1",
+            4: "3",
+            5: "2",
         },
         correctAnswer: 0,
         clickedAnswer: 0,
@@ -43,13 +57,13 @@ export default class Quiz extends Component {
     // the method that checks the correct answer
     checkAnswer = answer => {
         const { correctAnswers, step, score } = this.state;
-        if(answer === correctAnswers[step]){
+        if (answer === correctAnswers[step]) {
             this.setState({
                 score: score + 1,
                 correctAnswer: correctAnswers[step],
                 clickedAnswer: answer
             });
-        }else{
+        } else {
             this.setState({
                 correctAnswer: 0,
                 clickedAnswer: answer
@@ -66,11 +80,11 @@ export default class Quiz extends Component {
         });
     }
 
-    render(){
+    render() {
         let { quiestions, answers, correctAnswer, clickedAnswer, step, score } = this.state;
-        return(
+        return (
             <div className="Content">
-                {step <= Object.keys(quiestions).length ? 
+                {step <= Object.keys(quiestions).length ?
                     (<>
                         <Question
                             question={quiestions[step]}
@@ -83,12 +97,12 @@ export default class Quiz extends Component {
                             clickedAnswer={clickedAnswer}
                         />
                         <button
-                        className="NextStep"
-                        disabled={
-                            clickedAnswer && Object.keys(quiestions).length >= step
-                            ? false : true
-                        }
-                        onClick={() => this.nextStep(step)}>Next</button>
+                            className="NextStep"
+                            disabled={
+                                clickedAnswer && Object.keys(quiestions).length >= step
+                                    ? false : true
+                            }
+                            onClick={() => this.nextStep(step)}>Next</button>
                     </>) : (
                         <div className="finalPage">
                             <h1>You have completed the quiz!</h1>
